45325b5b67ef77dcb5e5acbb16e0ebe8fa15464b
Heather Peterson <hpeterson462@gmail.com>
2020-08-19 20:51:26 -0700

    data

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,53 +1,123 @@
 require('dotenv').config();
 
 const { execSync } = require('child_process');
-
 const fakeRequest = require('supertest');
 const app = require('../lib/app');
 const client = require('../lib/client');
 
-describe('app routes', () => {
-  beforeAll(done => {
-    return client.connect(done);
-  });
+describe('routes', () => {
+  let token;
+  const newTodo = {
+    name: 'wash dishes',
+    completed: true,
+    owner_id: 2,
+  };
 
-  beforeEach(() => {
-    // TODO: ADD DROP SETUP DB SCRIPT
+  beforeAll(async done => {
     execSync('npm run setup-db');
+    client.connect();
+    const signInData = await fakeRequest(app)
+      .post('/auth/signup')
+      .send({
+        email: 'jon@user.com',
+        password: '1234'
+      });
+    token = signInData.body.token;
+    return done();
   });
 
   afterAll(done => {
     return client.end(done);
   });
 
-  test('returns animals', async() => {
+  test('returns a new todo when creating new todos', async (done) => {
+    const data = await fakeRequest(app)
+      .post('/api/todos')
+      .send(newTodo)
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(newTodo);
+    done();
+  });
 
-    const expectation = [
+  test('returns all todos for the user when hitting GET /todos', async (done) => {
+    const expected = [
       {
-        'id': 1,
-        'name': 'bessie',
-        'coolfactor': 3,
-        'owner_id': 1
+        name: 'wash dishes',
+        completed: true,
+        owner_id: 2,
       },
-      {
-        'id': 2,
-        'name': 'jumpy',
-        'coolfactor': 4,
-        'owner_id': 1
-      },
-      {
-        'id': 3,
-        'name': 'spot',
-        'coolfactor': 10,
-        'owner_id': 1
-      }
     ];
 
     const data = await fakeRequest(app)
-      .get('/animals')
+      .get('/api/todos')
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expected);
+    done();
+  });
+
+  test('returns a single todo for the user when hitting GET /todos/:id', async (done) => {
+    const expected = {
+      name: 'wash dishes',
+      completed: true,
+      owner_id: 2,
+    };
+
+    const data = await fakeRequest(app)
+      .get('/api/todos/4')
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(expected);
+    done();
+  });
+
+  test('updates a single todo for the user when hitting PUT /todos/:id', async (done) => {
+    const newTodo = {
+      name: 'wash dishes',
+      completed: true,
+      owner_id: 2,
+    };
+
+    const expectedAlltodos = [{
+      name: 'wash dishes',
+      completed: true,
+      owner_id: 2,
+    }];
+
+    const data = await fakeRequest(app)
+      .put('/api/todos/4')
+      .send(newTodo)
+      .set('Authorization', token)
       .expect('Content-Type', /json/)
       .expect(200);
 
-    expect(data.body).toEqual(expectation);
+    const alltodos = await fakeRequest(app)
+      .get('/api/todos')
+      .send(newTodo)
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(newTodo);
+    expect(alltodos.body).toEqual(expectedAlltodos);
+    done();
+  });
+
+  test('delete a single todo for the user when hitting DELETE /todos/:id', async (done) => {
+    await fakeRequest(app)
+      .delete('/api/todos/4')
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    const data = await fakeRequest(app)
+      .get('/api/todos/')
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual([]);
+    done();
   });
 });
